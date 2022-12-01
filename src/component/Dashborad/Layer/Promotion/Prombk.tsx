83f68ba6d559b8d32d152eaad2465c23fb35c8a6
shahrukhnasir <shahrukhnasir0108@gmail.com>
2022-12-01 19:24:51 +0500

    upadated

--- src/component/Dashborad/Layer/Promotion/Prombk.tsx
+++ src/component/Dashborad/Layer/Promotion/Prombk.tsx
@@ -28,7 +28,7 @@ const  Promotion =() =>{
     <>
         <TopBar/>
  <SideBar />
- <div>Text here</div>
+ <div>TExt here</div>
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
